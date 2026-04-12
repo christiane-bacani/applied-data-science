f5d9cf64a556b6bef279723bcb38d50b992b387a
christiane-bacani <christianebacani@outlook.com>
2026-04-13 00:14:45 +0800

    Added new module

---
+++ courses/datacamp-courses/Introduction-to-NumPy/Understanding-NumPy-Arrays/introducing-arrays/Exercises/creating_arrays_from_scratch.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Create an array of zeros which has four columns and two rows
+zero_array = np.zeros(shape=(2, 4))
+print(zero_array)
+
+# Create an array of random floats which has six columns and three rows
+random_array = np.random.random((3, 6))
+print(random_array)
