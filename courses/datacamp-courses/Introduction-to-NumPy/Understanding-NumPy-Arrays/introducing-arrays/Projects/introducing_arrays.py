bc2c06fda3177a62511350358182bcfce3b7ec4d
christiane-bacani <christianebacani@outlook.com>
2026-04-20 12:11:41 +0800

    Update the content of the module

--- courses/datacamp-courses/Introduction-to-NumPy/Understanding-NumPy-Arrays/introducing-arrays/Projects/introducing_arrays.py
+++ courses/datacamp-courses/Introduction-to-NumPy/Understanding-NumPy-Arrays/introducing-arrays/Projects/introducing_arrays.py
@@ -1,11 +1,24 @@
 import pandas as pd
 import numpy as np
 
-def create_oned_arr(df: pd.Series):
+def create_oned_arr(df: pd.Series) -> np.array:
+    """Create one-dimensional array using NumPy Package."""
     oned_arr = np.array(df)
     return oned_arr
+
+def create_twod_arr(df1: pd.Series, df2: pd.Series) -> np.array:
+    """Create two-dimensional array using NumPy Package."""
+    twod_arr = np.column_stack([df1, df2])
+    return twod_arr
 
 df = pd.read_csv("courses/datacamp-courses/Introduction-to-NumPy/Understanding-NumPy-Arrays/introducing-arrays/Datasets/mental_health.csv")
 
 person_id = create_oned_arr(df["Person_ID"])
+print(f"One-dimensional Array:")
 print(person_id)
+
+print()
+
+person_id_and_age = create_twod_arr(df["Person_ID"], df["Age"])
+print(f"Two-dimensional Array:")
+print(person_id_and_age)
