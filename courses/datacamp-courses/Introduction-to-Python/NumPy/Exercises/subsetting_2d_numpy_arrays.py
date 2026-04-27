30a00c7079786ed2cd65b6ba9520f4ca1eadfdc9
christiane-bacani <christianebacani@outlook.com>
2026-04-27 14:22:25 +0800

    feat: New module

---
+++ courses/datacamp-courses/Introduction-to-Python/NumPy/Exercises/subsetting_2d_numpy_arrays.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:, 1]
+
+# Print out height of 124th player
+print(np_baseball[123, 0])
