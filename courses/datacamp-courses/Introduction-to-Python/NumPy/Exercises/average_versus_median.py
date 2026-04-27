539f1e6a7e2490bc66c02ebf7689895ac3f929b4
christiane-bacani <christianebacani@outlook.com>
2026-04-27 14:29:02 +0800

    feat: New module

---
+++ courses/datacamp-courses/Introduction-to-Python/NumPy/Exercises/average_versus_median.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+baseball = baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+np_baseball = np.array(baseball)
+
+# Create np_height_in from np_baseball
+np_height_in = np.array(np_baseball[:, 0])
+
+# Print out the mean of np_height_in
+print(np.mean(np_height_in))
+
+# Print out the median of np_height_in
+print(np.median(np_height_in))
